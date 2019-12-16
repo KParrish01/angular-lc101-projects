6301b732b16535683e063e266bd2d5af16b3b788
KParrish01 <klaraparrish@yahoo.com>
2019-12-16 12:32:33 -0600

    This is it for Exercise on Angular 1 lesson

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = [
+              'https://education.launchcode.org/intro-to-professional-web-dev/index.html', 
+             'https://education.launchcode.org/intro-to-professional-web-dev/chapters/angular-lsn1/exercises.html'
+            ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'And Here Some Pics!';
+  photosTitle = 'Random Pics!';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
   image2 = 'http://www.gstatic.com/tv/thumb/v22vodart/22005/p22005_v_v8_ad.jpg';
   image3 = 'https://cdn.vox-cdn.com/thumbor/UqY0xeSxq8K3LmfXy7oiErj1u9Q=/0x0:1024x768/1200x800/filters:focal(431x203:593x365)/cdn.vox-cdn.com/uploads/chorus_image/image/51816113/thegilmoregirls.0.0.jpg';
